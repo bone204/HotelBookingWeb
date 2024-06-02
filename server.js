1f95d6e8686e75e184fabebf33636143b42e15c1
bone204 <truongbmt4@gmail.com>
2024-06-02 09:31:55 +0700

    Up github

---
+++ server.js
@@ -0,0 +1,80 @@
+const express = require('express');
+const app = express();
+
+const sql = require('msnodesqlv8');
+const connectionString = "server=.;Database=QLKS_LAOPERA;Trusted_Connection=Yes;Driver={SQL Server Native Client 11.0}";
+const kh = "SELECT * FROM KHACHHANG";
+const p = "SELECT MALOAIP, COUNT(*) AS AVAILABLE_ROOMS FROM PHIEUDATPHONG WHERE NGAYNHAN GROUP BY MALOAIP";
+
+sql.query(connectionString, kh, (err, rows) => {
+    console.log(rows);
+})
+
+app.use('/assets', express.static(__dirname + '/assets'));
+
+app.listen(3000);
+
+app.use(express.json());
+
+app.get("/home", (req,res) => {
+    res.sendFile("index.html", {root: __dirname});
+})
+
+app.get("/rooms", (req, res) => {
+    sql.query(connectionString, p, (err, rows) => {
+        if (err) {
+            console.error("Error querying rooms:", err);
+            res.status(500).json({ error: "Error querying rooms" });
+        } else {
+            res.status(200).json(rows); // Gửi kết quả về dưới dạng JSON
+        }
+    });
+});
+
+app.post("/contact", (req, res) => {
+    // Lấy dữ liệu từ req.body gửi từ frontend
+    const { fullName, identity, sex, birthDate, email, phone, address, nationality } = req.body;
+
+    // Tạo câu truy vấn SQL để chèn dữ liệu vào bảng KHACHHANG
+    const insertQuery = `
+        INSERT INTO KHACHHANG (TENKH, CCCD, GIOITINH, NGAYSINH, EMAIL, LOAIKH, SDT, DIACHI, QUOCTICH, TINHTRANG)
+        VALUES (?, ?, ?, ?, ?, 1, ?, ?, ?, 1)
+    `;
+
+    // Mảng các tham số cho câu truy vấn SQL
+    const params = [fullName, identity, sex, birthDate, email, phone, address, nationality];
+
+    // Thực thi truy vấn SQL
+    sql.query(connectionString, insertQuery, params, (err, result) => {
+        if (err) {
+            console.error("Error inserting data:", err);
+            res.status(500).send("Error inserting data into database");
+        } else {
+            console.log("Data inserted successfully");
+            res.status(200).send("Data inserted successfully");
+        }
+    });
+});
+
+app.post("/contact1", (req, res) => {
+    const { arrivalInput, departureInput } = req.body;
+
+    // Tạo câu truy vấn SQL để chèn dữ liệu vào bảng KHACHHANG
+    const insertQuery = `
+        SELECT MALOAIP, COUNT(*) AS AVAILABLE_ROOMS FROM PHIEUDATPHONG WHERE NGAYNHAN GROUP BY MALOAIP
+    `;
+
+    // Mảng các tham số cho câu truy vấn SQL
+    const params = [arrivalInput, departureInput];
+
+    // Thực thi truy vấn SQL
+    sql.query(connectionString, insertQuery, params, (err, result) => {
+        if (err) {
+            console.error("Error inserting data:", err);
+            res.status(500).send("Error inserting data into database");
+        } else {
+            console.log("Data inserted successfully");
+            res.status(200).send("Data inserted successfully");
+        }
+    });
+});
